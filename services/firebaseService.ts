ed026796f2944cd1bfc05d501c08ce1953733293
Leonardo Adi Orta <adiorta@gmail.com>
2025-12-11 17:35:41 +0700

    Update firebaseService.ts

--- services/firebaseService.ts
+++ services/firebaseService.ts
@@ -1,20 +1,12 @@
-// Import fungsi yang dibutuhkan SDK (Sesuai dokumentasi resmi)
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBNsY1YTvbBX82ua41yUNiI1dNQEulaATg",
   authDomain: "sewa-kendaraan-283af.firebaseapp.com",
   projectId: "sewa-kendaraan-283af",
   storageBucket: "sewa-kendaraan-283af.firebasestorage.app",
   messagingSenderId: "1033232570934",
-  appId: "1:1033232570934:web:8a1563bb84deafccd29697",
-  measurementId: "G-BW9MHSRB6N"
+  appId: "1:1033232570934:web:8a1563bb84deafccd29697"
 };
-
-// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inisialisasi Firestore Database
 export const db = getFirestore(app);
